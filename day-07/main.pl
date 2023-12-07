da450616f5820613d0f9c69966fd4cb5aab613a7
Huterok228 <clash.of.clans23916@gmail.com>
2023-12-08 00:05:14 +0300

    formatted day-07

--- day-07/main.pl
+++ day-07/main.pl
@@ -1,142 +1,146 @@
 #!/usr/bin/env perl
 
+# perl ./main.pl
+
 use strict;
 use warnings;
 
 sub findTotalWinnings {
-	my $input = shift;
+  my $input = shift;
 
-	my ($handsRef, $bidsRef) = retrieveHandsAndBids($input);
-	my $withJokers = 0;
+  my ($handsRef, $bidsRef) = retrieveHandsAndBids($input);
+  my $withJokers = 0;
 
-	my @sortedHands = sortHands($handsRef, $bidsRef, $withJokers);
+  my @sortedHands = sortHands($handsRef, $bidsRef, $withJokers);
 
-	my $res = 0;
-	for(my $i = 0; $i < $#sortedHands + 1; $i++) {
-		$res += ($i + 1) * $sortedHands[$i]->{bid};
-	}
-	return $res;
+  my $res = 0;
+  for(my $i = 0; $i <= $#sortedHands; $i++) {
+    $res += ($i + 1) * $sortedHands[$i]->{bid};
+  }
+
+  return $res;
 }
 
 sub findTotalJokerWinnings {
-	my $input = shift;
+  my $input = shift;
 
-	my ($handsRef, $bidsRef) = retrieveHandsAndBids($input);
-	my $withJokers = 1;
+  my ($handsRef, $bidsRef) = retrieveHandsAndBids($input);
+  my $withJokers = 1;
 
-	my @sortedHands = sortHands($handsRef, $bidsRef, $withJokers);
+  my @sortedHands = sortHands($handsRef, $bidsRef, $withJokers);
 
-	my $res = 0;
-	for(my $i = 0; $i < $#sortedHands + 1; $i++) {
-		$res += ($i + 1) * $sortedHands[$i]->{bid};
-	}
-	return $res;
+  my $res = 0;
+  for(my $i = 0; $i <= $#sortedHands; $i++) {
+    $res += ($i + 1) * $sortedHands[$i]->{bid};
+  }
+
+  return $res;
 }
 
 sub retrieveHandsAndBids {
-	my $input = shift;
-	my @lines = split "\n", $input;
+  my $input = shift;
+  my @lines = split "\n", $input;
 
-	my @hands;
-	my @bids;
+  my @hands;
+  my @bids;
 
-	foreach my $line (@lines) {
-		my ($hand, $bid) = split ' ', $line;
-		push @hands, $hand;
-		push @bids, $bid;
-	}
+  foreach my $line (@lines) {
+    my ($hand, $bid) = split ' ', $line;
+    push @hands, $hand;
+    push @bids, $bid;
+  }
 
-	return (\@hands, \@bids);
+  return (\@hands, \@bids);
 }
 
 sub sortHands {
-	my ($handsRef, $bidsRef, $withJokers) = @_;
+  my ($handsRef, $bidsRef, $withJokers) = @_;
 
-	my @tuples;
-	for(my $i = 0; $i < @$handsRef; $i++) {
-		push @tuples, {hand => $handsRef->[$i], bid => $bidsRef-> [$i]};
-	}
+  my @tuples;
+  for(my $i = 0; $i < @$handsRef; $i++) {
+    push @tuples, {hand => $handsRef->[$i], bid => $bidsRef-> [$i]};
+  }
 
-	@tuples = sort {
-			my $strengthCmp = findHandStrength($a->{hand}, $withJokers) <=> findHandStrength($b->{hand}, $withJokers);
-			return $strengthCmp if $strengthCmp != 0;
-			my $mismatchCompare = compareFirstHandsMismatch($a->{hand}, $b->{hand}, $withJokers);
-			return $mismatchCompare;
-	} @tuples;
+  @tuples = sort {
+    my $strengthCmp = findHandStrength($a->{hand}, $withJokers) <=> findHandStrength($b->{hand}, $withJokers);
+    return $strengthCmp if $strengthCmp != 0;
+    my $mismatchCompare = compareFirstHandsMismatch($a->{hand}, $b->{hand}, $withJokers);
+    return $mismatchCompare;
+  } @tuples;
 
-	return @tuples;
+  return @tuples;
 }
 
 sub findHandStrength {
-	my ($hand, $withJokers)= @_;
+  my ($hand, $withJokers)= @_;
 
-	my $jokers = 0;
-	my %letterHashes;
-	foreach my $letter (split "", $hand) {
-		if($withJokers && $letter eq 'J') {
-			$jokers += 1;
-			next;
-		}
-		$letterHashes{$letter}++;
-	}
+  my $jokers = 0;
+  my %letterHashes;
+  foreach my $letter (split "", $hand) {
+    if($withJokers && $letter eq 'J') {
+      $jokers += 1;
+      next;
+    }
+    $letterHashes{$letter}++;
+  }
 
-	my $max = 0;
-	my $threes = 0;
-	my $pair = 0;
+  my $max = 0;
+  my $threes = 0;
+  my $pair = 0;
 
-	foreach my $value (values %letterHashes) {
-		$max = $value + $jokers if $value + $jokers > $max;
+  foreach my $value (values %letterHashes) {
+    $max = $value + $jokers if $value + $jokers > $max;
 
-		if ($value + $jokers == 3) {
-			$threes = 1;
-		} elsif ($value + $jokers == 2) {
-			$pair += 1;
-		}
-	}
+    if ($value + $jokers == 3) {
+      $threes = 1;
+    } elsif ($value + $jokers == 2) {
+      $pair += 1;
+    }
+  }
 
-	return 6 if $max == 5;
-	return 5 if $max == 4;
-	return 4 if $threes && $pair;
-	return 3 if $threes;
-	return 2 if $pair == 2;
-	return 1 if $pair == 1;
-	return 0;
+  return 6 if $max == 5;
+  return 5 if $max == 4;
+  return 4 if $threes && $pair;
+  return 3 if $threes;
+  return 2 if $pair == 2;
+  return 1 if $pair == 1;
+  return 0;
 }
 
 sub compareFirstHandsMismatch {
-	my ($hand1, $hand2, $withJokers) = @_;
+  my ($hand1, $hand2, $withJokers) = @_;
 
-	for (my $i = 0; $i < length($hand1); $i++) {
-			my $val1 = convertCharToNum(substr($hand1, $i, 1), $withJokers);
-			my $val2 = convertCharToNum(substr($hand2, $i, 1), $withJokers);
+  for (my $i = 0; $i < length($hand1); $i++) {
+    my $val1 = convertCharToNum(substr($hand1, $i, 1), $withJokers);
+    my $val2 = convertCharToNum(substr($hand2, $i, 1), $withJokers);
+    
+    if ($val1 ne $val2) {
+      return $val1 <=> $val2;
+    }
+  }
 
-			if ($val1 ne $val2) {
-				return $val1 <=> $val2;
-			}
-		}
-
-		return -1;
+  return -1;
 }
 
 sub convertCharToNum {
-	my ($card, $with_jokers) = @_;
+  my ($card, $with_jokers) = @_;
 
-	my $val;
-	if ($card eq 'A') {
-		$val = 12;
-	} elsif ($card eq 'K') {
-		$val = 11;
-	} elsif ($card eq 'Q') {
-		$val = 10;
-	} elsif ($card eq 'J') {
-		$val = $with_jokers ? 0 : 9;
-	} elsif ($card eq 'T') {
-		$val = $with_jokers ? 9 : 8;
-	} else {
-		$val = ord($card) - ord('0') - ($with_jokers ? 1 : 2);
-	}
+  my $val;
+  if ($card eq 'A') {
+    $val = 13;
+  } elsif ($card eq 'K') {
+    $val = 12;
+  } elsif ($card eq 'Q') {
+    $val = 11;
+  } elsif ($card eq 'J') {
+    $val = $with_jokers ? 1 : 10;
+  } elsif ($card eq 'T') {
+    $val = $with_jokers ? 10 : 9;
+  } else {
+    $val = ord($card) - ord('0') - ($with_jokers ? 0 : 1);
+  }
 
-	return $val;
+  return $val;
 }
 
 open(our $file, "<", "example_input.txt") or die "RETARDED!!!";
