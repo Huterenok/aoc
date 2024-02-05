003961caa81fea25e9fd4ec668b8f1a76470eb40
Huterenok <clash.of.clans23916@gmail.com>
2024-02-05 23:58:34 +0300

    2022-day-03(fix)

--- rusty-2021/day-03/src/lib.rs
+++ rusty-2021/day-03/src/lib.rs
@@ -1,58 +1,52 @@
-pub fn find_gamma_and_epsilon(input: &str) -> usize {
-    let bits = input
-        .lines()
-        .flat_map(|line| line.chars().map(|s| s.to_digit(10).unwrap()))
-        .collect::<Vec<_>>();
-    let rate_len = input.lines().next().unwrap().len();
-    let rate_count = input.lines().count();
+use std::cmp::Ordering;
 
-    let (gamma, epsilon) = (0..rate_len).fold((0, 0), |(g, e), col| {
-        let bit = find_bit(&bits, rate_count, rate_len, col);
-        if bit == 1 {
-            (g + (1 << (rate_len - col - 1)) as usize, e)
-        } else {
-            (g, e + (1 << (rate_len - col - 1)) as usize)
-        }
-    });
-    gamma * epsilon
+pub fn epsilon_x_gamma(input: &str) -> usize {
+    let nums = input
+        .lines()
+        .map(|line| usize::from_str_radix(line, 2).unwrap())
+        .collect::<Vec<_>>();
+    let num_len = input.lines().next().unwrap().len();
+
+    let gamma = (0..num_len)
+        .map(|i| match find_bit(&nums, i) {
+            Ordering::Greater => 1 << i,
+            _ => 0,
+        })
+        .sum::<usize>();
+
+    gamma * (!gamma & ((1 << num_len) - 1))
 }
 
-//TODO
-pub fn find_life_support_rating(input: &str, oxygen: u32) -> usize {
-    let mut bits = input
+pub fn with_oxygen(input: &str, oxygen: usize) -> usize {
+    let mut nums = input
         .lines()
-        .flat_map(|line| line.chars().map(|s| s.to_digit(10).unwrap()))
+        .map(|line| usize::from_str_radix(line, 2).unwrap())
         .collect::<Vec<_>>();
-    let rate_len = input.lines().next().unwrap().len();
+    let num_len = input.lines().next().unwrap().len();
 
-    for i in 0..rate_len {
-        let bit = find_bit(&bits, bits.len() / rate_len, rate_len, i) ^ oxygen;
-        bits = bits
-            .chunks(rate_len)
-            .filter(|b| b[i] == bit)
-            .flatten()
-            .copied()
-            .collect();
-        if bits.len() == rate_len {
+    for i in (0..num_len).rev() {
+        let bit = match find_bit(&nums, i) {
+            Ordering::Equal => oxygen,
+            Ordering::Less => 1 ^ oxygen,
+            Ordering::Greater => 0 ^ oxygen,
+        };
+        nums.retain(|x| (x >> i) & 1 == bit);
+        if nums.len() == 1 {
             break;
         }
     }
-
-    bits.into_iter()
-        .rev()
-        .enumerate()
-        .fold(0, |acc, (i, e)| acc + e as usize * (1 << i))
+    nums[0]
 }
 
-pub fn find_bit(bits: &Vec<u32>, rate_count: usize, rate_len: usize, col: usize) -> u32 {
-    let power =
-        (0..rate_count).fold(0, |stage_acc, idx| stage_acc + bits[col + rate_len * idx]) as usize;
-
-    if power > rate_count / 2 {
-        1
-    } else {
-        0
-    }
+fn find_bit(nums: &[usize], bit: usize) -> Ordering {
+    let (x, y) = nums.iter().fold((0, 0), |(x, y), num| {
+        if (num >> bit) & 1 == 1 {
+            (x + 1, y)
+        } else {
+            (x, y + 1)
+        }
+    });
+    x.cmp(&y)
 }
 
 #[cfg(test)]
@@ -65,14 +59,13 @@ mod tests {
         let example_input = fs::read_to_string("./example_input.txt").unwrap();
         let input = fs::read_to_string("./input.txt").unwrap();
 
-        let res1_example = find_gamma_and_epsilon(&example_input);
-        let res1 = find_gamma_and_epsilon(&input);
+        let res1_example = epsilon_x_gamma(&example_input);
+        let res1 = epsilon_x_gamma(&input);
         assert_eq!(res1_example, 198);
         assert_eq!(res1, 2498354);
 
-        let res2_example = find_life_support_rating(&example_input, 1)
-            * find_life_support_rating(&example_input, 0);
-        let res2 = find_life_support_rating(&input, 0) * find_life_support_rating(&input, 1);
+        let res2_example = with_oxygen(&example_input, 0) * with_oxygen(&example_input, 1);
+        let res2 = with_oxygen(&input, 0) * with_oxygen(&input, 1);
         assert_eq!(res2_example, 230);
         assert_eq!(res2, 3277956);
     }
